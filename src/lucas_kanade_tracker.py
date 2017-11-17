c4404d135af816b788c0a37441d6d0712784de44
vietthang207 <vietthang207@gmail.com>
2017-11-17 20:37:50 +0800

    add video writer code

--- src/lucas_kanade_tracker.py
+++ src/lucas_kanade_tracker.py
@@ -10,12 +10,8 @@ def pyramid_reduce(frame):
 	ker1d = np.array([[0.25-a/2, 0.25, a, 0.25, 0.25-a/2]])
 	kernel = np.kron(ker1d, ker1d.transpose())
 	kernel = np.array([[1]])
-	kernel = gaussWin(13).dot(gaussWin(13).transpose())
+	# kernel = gaussWin(13).dot(gaussWin(13).transpose())
 	smoothened_frame = (conv2d(frame, kernel, 'same')).astype(np.float32)
-	# for i in range(sz[0]):
-	# 	for j in range(sz[1]):
-	# 		if (smoothened_frame[i][j] != frame[i][j]):
-	# 			print("diff", i, j, smoothened_frame[i][j], frame[i][j])
 	return smoothened_frame[::2, ::2]
 
 def gen_pyramid(frame, max_level):
@@ -34,8 +30,12 @@ def bitmap_to_feature_list(bitmap):
 			if bitmap[i][j] != 0:
 				feature_list[c] = [i, j]
 				c += 1
-	print(c)
 	return feature_list
+
+def find_good_features(frame):
+	feature_list = corner_detector(frame, 7, 7)
+	p0 = bitmap_to_feature_list(feature_list)
+	return p0
 
 def lk_pyr(frame1, frame2, kernel, coordinate):
 	frame_height = frame1.shape[0]
@@ -43,7 +43,7 @@ def lk_pyr(frame1, frame2, kernel, coordinate):
 	kernel_size = kernel.shape[0]
 	min_img_size = 16
 	max_pyr_level = int((np.log2(min(frame_width, frame_height)/min_img_size)))
-	max_pyr_level = 3
+	max_pyr_level = Constant.MAX_PYRAMID_LEVEL 
 	# print("max_pyr_level", max_pyr_level)
 	pyramid1 = gen_pyramid(frame1, max_pyr_level)
 	pyramid2 = gen_pyramid(frame2, max_pyr_level)
@@ -55,8 +55,11 @@ def lk_pyr(frame1, frame2, kernel, coordinate):
 		frame2 = pyramid2[level]
 		scaled_coordinate = coordinate * 1.0 / pow(2, level)
 		scaled_coordinate2 = (coordinate + velocity * 2) / pow(2, level)
-		addition_velocity, _ = lk_one_level(frame1, frame2, kernel, scaled_coordinate, scaled_coordinate2)
+		addition_velocity, st1 = lk_one_level(frame1, frame2, kernel, scaled_coordinate, scaled_coordinate2)
 		velocity = velocity * 2 + addition_velocity
+		for i in range(st.shape[0]):
+			if st1[i] == 0:
+				st[i] = 0
 	return coordinate + velocity, st
 
 def lk_one_level(frame1, frame2, kernel, coordinate, coordinate2):
@@ -104,8 +107,6 @@ def lk_one_level(frame1, frame2, kernel, coordinate, coordinate2):
 				b2 += It_w[i1, j1] * Iy_w[i1, j1] * kernel[i1, j1]
 		Z = np.array([[z1, z2], [z3, z4]])
 		B = np.array([[b1], [b2]])
-		# velocity[k, 0, 0] = velocity[k, 0, 0]*2
-		# velocity[k, 0, 1] = velocity[k, 0, 1]*2
 		try:
 			v = np.dot(np.linalg.inv(Z), B)
 			velocity[k, 0, 0] = v[0]
@@ -113,28 +114,22 @@ def lk_one_level(frame1, frame2, kernel, coordinate, coordinate2):
 		except np.linalg.linalg.LinAlgError:
 			st[k] = 0
 		
-		# velocity[k, 0, 0] = 0
-		# velocity[k, 0, 1] = 1
-
-		# velocity[k, 0, 0] = velocity[k, 0, 0]*2 + v[0]
-		# velocity[k, 0, 1] = velocity[k, 0, 1]*2 + v[1]
-		
-	# output = (coordinate + velocity)
 	return velocity, st
 				
 if __name__ == "__main__":
-	cap = cv2.VideoCapture('../videos/traffic.mp4')
+	cap = cv2.VideoCapture('../videos/bird.mp4')
 	ret, old_frame = cap.read()
 	old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+	width = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
+	height = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+	fourcc = cv2.cv.CV_FOURCC(*'XVID')
+	writer = cv2.VideoWriter('../videos/bird_output.avi', fourcc, 20.0, (width, height))
+
 	kernel = gaussWin(13).dot(gaussWin(13).transpose())
 	c = 0
 	for i in range(kernel.shape[0]):
 		for j in range(kernel.shape[0]):
 			c += kernel[i, j]
-	print(c)
-	print(kernel)
-	# feature_list = corner_detector(old_gray, 7, 7)
-	# p0 = bitmap_to_feature_list(feature_list)
 	lk_params = dict( winSize  = (15,15),
                   maxLevel = 2,
                   criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
@@ -142,19 +137,23 @@ if __name__ == "__main__":
                        qualityLevel = 0.3,
                        minDistance = 7,
                        blockSize = 7 )
-	p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+	
 	# print("shape", p0.shape)
 	color = np.random.randint(0,255,(Constant.NUM_FEATURE_TO_TRACK,3))
 	mask = np.zeros_like(old_frame)
+	counter = 0;
 	while(1):
+	    if counter % Constant.FEATURE_RESET_INTERVAL == 0:
+	        p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+	        # p0 = find_good_features(old_gray)
+	        mask = np.zeros_like(old_frame)
+	    counter += 1
 	    ret,frame = cap.read()
 	    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	    cv2.imwrite("frameee1.jpg", frame_gray)
-	    # print(frame_gray.shape)
 	    # calculate optical flow
 	    # p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 	    p1, st = lk_pyr(old_gray, frame_gray, kernel, p0)
-	    # break
 	    # Select good points
 	    good_new = p1[st==1]
 	    good_old = p0[st==1]
@@ -165,6 +164,7 @@ if __name__ == "__main__":
 	        cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
 	        cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
 	    img = cv2.add(frame,mask)
+	    writer.write(img)
 	    cv2.imshow('frame',img)
 	    k = cv2.waitKey(30) & 0xff
 	    if k == 27:
@@ -174,28 +174,5 @@ if __name__ == "__main__":
 	    p0 = good_new.reshape(-1,1,2)
 	cv2.destroyAllWindows()
 	cap.release()
-	# ret, frame2 = cap.read()
-	# frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-	# cv2.imshow('frame1', frame1)
-	# cv2.waitKey()
+	writer.release()
 	
-	# print(kernel.shape)
-	# blur = gen_pyramid(frame1, 5)
-	# for i in range(len(blur)):
-	# 	cv2.imshow("blur " + str(i), blur[i])
-	# 	cv2.waitKey()
-
-	# tracker = LucasKanadeTracker()
-	# vx, vy = tracker.calculate_velocity(frame1, frame2, kernel)
-	# frame_height = frame1.shape[0]
-	# frame_width = frame1.shape[1]
-	# print(frame1.shape)
-	# for i in range(frame_height):
-	# 	for j in range(frame_width):
-	# 		v = np.sqrt(vx[i][j]*vx[i][j] + vy[i][j]*vy[i][j])
-	# 		if v > 0.5:
-	# 			frame1[i][j] = 0
-
-	# print(frame1.shape)
-	# cv2.imshow('frame1 velo', frame1)
-	# cv2.waitKey()
